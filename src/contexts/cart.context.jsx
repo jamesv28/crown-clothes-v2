0148d793dc709b0307927335bdeebca1288c94c0
James Volmert <james.volmert@gmail.com>
2024-04-30 17:25:43 -0600

    added correct quantity to cart icon, started checkout view ui wiring

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 
 export const addCartItem = (cartItems, productToAdd) => {
   const existingCartItem = cartItems.find(
@@ -21,16 +21,31 @@ export const CartContext = createContext({
   setIsOpen: () => {},
   cartItems: [],
   addItemToCart: () => {},
+  cartCount: 0,
 });
 
 export const CartProvider = ({ children }) => {
   const [isCartOpen, setIsCartOpen] = useState(false);
   const [cartItems, setCartItems] = useState([]);
-
-  const addItemToCart = (product) =>
+  const [cartCount, setCartCount] = useState(0);
+  const addItemToCart = (product) => {
     setCartItems(addCartItem(cartItems, product));
+  };
 
-  const value = { isCartOpen, setIsCartOpen, cartItems, addItemToCart };
+  useEffect(() => {
+    const newCartCount = cartItems.reduce(
+      (total, currentCartItem) => total + currentCartItem.quantity,
+      0
+    );
+    setCartCount(newCartCount);
+  }, [cartItems]);
+  const value = {
+    isCartOpen,
+    setIsCartOpen,
+    cartItems,
+    addItemToCart,
+    cartCount,
+  };
 
   return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
 };
