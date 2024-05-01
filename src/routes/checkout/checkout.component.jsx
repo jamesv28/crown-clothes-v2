aece6c0fe960864137cc070ac82c30d1017ff7ba
James Volmert <james.volmert@gmail.com>
2024-05-01 16:48:44 -0600

    checkout item component and checkout page

--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -1,28 +1,34 @@
-import React from "react";
-import CartItem from "../../components/cart-item/cart-item.component";
 import { useContext } from "react";
 import { CartContext } from "../../contexts/cart.context";
-
+import CheckoutItem from "../../components/checkout-item/checkout-item.component";
 import "./checkout.styles.scss";
 
 const Checkout = () => {
   const { cartItems, addItemToCart, removeItemFromCart } =
     useContext(CartContext);
   return (
-    <div>
-      <h1>Checkout page</h1>
-      {cartItems.map((item) => {
-        const { name, quantity, id } = item;
-
-        return (
-          <div key={id}>
-            <h3>{name}</h3>
-            <button onClick={() => removeItemFromCart(item)}>decrement</button>
-            <span>{quantity}</span>
-            <button onClick={() => addItemToCart(item)}>increment</button>
-          </div>
-        );
+    <div className="checkout-container">
+      <div className="checkout-header">
+        <div className="header-block">
+          <span>Product</span>
+        </div>
+        <div className="header-block">
+          <span>Description</span>
+        </div>
+        <div className="header-block">
+          <span>Quantity</span>
+        </div>
+        <div className="header-block">
+          <span>Price</span>
+        </div>
+        <div className="header-block">
+          <span>Remove</span>
+        </div>
+      </div>
+      {cartItems.map((cartItem) => {
+        return <CheckoutItem cartItem={cartItem} key={cartItem.id} />;
       })}
+      <span className="total">0</span>
     </div>
   );
 };
