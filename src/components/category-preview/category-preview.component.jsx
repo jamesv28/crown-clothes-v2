e5fd89eb1dddfc45e8ff87f1ed6ca61a76c1fab2
James Volmert <james.volmert@gmail.com>
2024-05-04 16:51:27 -0600

    more styled components

--- src/components/category-preview/category-preview.component.jsx
+++ src/components/category-preview/category-preview.component.jsx
@@ -1,23 +1,24 @@
 import ProductCard from "../product-card/product-card.component";
-import { Link } from "react-router-dom";
-import "./category-preview.styles.scss";
+import {
+  Preview,
+  CategoryTitle,
+  CategoryPreviewContainer,
+} from "./category-preview.styles";
 
 const CategoryPreview = ({ title, products }) => {
   return (
-    <div className="category-preview-container">
+    <CategoryPreviewContainer>
       <h2>
-        <Link className="title" to={title}>
-          {title.toUpperCase()}
-        </Link>
+        <CategoryTitle to={title}>{title}</CategoryTitle>
       </h2>
-      <div className="preview">
+      <Preview>
         {products
           .filter((_, idx) => idx < 4)
           .map((product) => (
             <ProductCard key={product.id} product={product} />
           ))}
-      </div>
-    </div>
+      </Preview>
+    </CategoryPreviewContainer>
   );
 };
 
--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,7 +1,15 @@
 import { useContext } from "react";
 import { CartContext } from "../../contexts/cart.context";
 
-import "./checkout-item.styles.scss";
+import {
+  CheckoutItemContaienr,
+  ImageContainer,
+  CheckoutSpans,
+  RemoveButton,
+  QuantityBox,
+  ArrowBlocks,
+  ValueBox,
+} from "./checkout-item.styles";
 const CheckoutItem = ({ cartItem }) => {
   const { removeItemFromCart, addItemToCart, clearItemFromCart } =
     useContext(CartContext);
@@ -17,25 +25,19 @@ const CheckoutItem = ({ cartItem }) => {
     removeItemFromCart(cartItem);
   };
   return (
-    <div className="checkout-item-container">
-      <div className="image-container">
+    <CheckoutItemContaienr>
+      <ImageContainer>
         <img src={imageUrl} alt={name} />
-      </div>
-      <span className="name">{name}</span>
-      <span className="quantity">
-        <div className="arrow" onClick={reduceQuantity}>
-          &#10094;
-        </div>
-        <span className="value">{quantity}</span>
-        <div className="arrow" onClick={addQuantity}>
-          &#10095;
-        </div>
-      </span>
-      <span className="price">{price}</span>
-      <div className="remove-button" onClick={clearItem}>
-        &#10005;
-      </div>
-    </div>
+      </ImageContainer>
+      <CheckoutSpans>{name}</CheckoutSpans>
+      <QuantityBox>
+        <ArrowBlocks onClick={reduceQuantity}>&#10094;</ArrowBlocks>
+        <ValueBox>{quantity}</ValueBox>
+        <ArrowBlocks onClick={addQuantity}>&#10095;</ArrowBlocks>
+      </QuantityBox>
+      <CheckoutSpans>{price}</CheckoutSpans>
+      <RemoveButton onClick={clearItem}>&#10005;</RemoveButton>
+    </CheckoutItemContaienr>
   );
 };
 
