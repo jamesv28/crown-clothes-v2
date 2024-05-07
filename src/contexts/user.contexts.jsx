65dbdf13098172e94a8f3ceea0e9f34b78757b7b
James Volmert <james.volmert@gmail.com>
2024-05-06 18:31:23 -0600

    finished reducers

--- src/contexts/user.contexts.jsx
+++ src/contexts/user.contexts.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState, useEffect } from "react";
+import { createContext, useEffect, useReducer } from "react";
 
 import {
   onAuthStateChangedListener,
@@ -10,10 +10,40 @@ export const UserContext = createContext({
   currentUser: null,
   setCurrentUser: () => null,
 });
+export const USER_ACTION_TYPES = {
+  SET_CURRENT_USER: "SET_CURRENT_USER",
+};
 
+const UserReducer = (state, action) => {
+  const { type, payload } = action;
+  switch (type) {
+    case USER_ACTION_TYPES.SET_CURRENT_USER:
+      return {
+        ...state,
+        currentUser: payload,
+      };
+    default:
+      throw new Error(`Unhandled type: ${type} in payload`);
+  }
+};
+
+export const INITIAL_STATE = {
+  currentUser: null,
+};
 // Provider that will wrap around other components
 export const UserProvider = ({ children }) => {
-  const [currentUser, setCurrentUser] = useState(null);
+  // const [currentUser, setCurrentUser] = useState(null);
+  const [state, dispatch] = useReducer(UserReducer, INITIAL_STATE);
+
+  // or we could destructure state
+  // const [{currentUser}, dispatch] = useReducer(UserReducer, INITIAL_STATE);
+
+  const { currentUser } = state;
+  console.log("current user", currentUser);
+  const setCurrentUser = (user) => {
+    dispatch({ type: USER_ACTION_TYPES.SET_CURRENT_USER, payload: user });
+  };
+
   const value = { currentUser, setCurrentUser };
 
   useEffect(() => {
